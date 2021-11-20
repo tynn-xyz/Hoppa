7fd5562d4600ef24f364265d772340e634d152e5
Christian Schmitz <christian@tynn.xyz>
2021-11-20 10:04:08 +0100

    Added collectLatest

--- flow/src/main/kotlin/xyz/tynn/hoppa/flow/Collect.kt
+++ flow/src/main/kotlin/xyz/tynn/hoppa/flow/Collect.kt
@@ -9,17 +9,32 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.launch
 
 /**
- * A shorthand for `flow.onEach { action(it) }.launchIn(scope)`
+ * A shorthand for `flow.map(action).launchIn(scope)`
  *
  * @see launchIn
+ * @see collect
  */
 public inline fun <T> Flow<T>.collectIn(
     scope: CoroutineScope,
     crossinline action: suspend (value: T) -> Unit,
 ): Job = scope.launch {
-    collect(action)
+    collect { action(it) }
+}
+
+/**
+ * A shorthand for `flow.mapLatest(action).launchIn(scope)`
+ *
+ * @see launchIn
+ * @see collectLatest
+ */
+public inline fun <T> Flow<T>.collectLatestIn(
+    scope: CoroutineScope,
+    crossinline action: suspend (value: T) -> Unit,
+): Job = scope.launch {
+    collectLatest { action(it) }
 }
--- flow/src/test/kotlin/xyz/tynn/hoppa/flow/CollectTest.kt
+++ flow/src/test/kotlin/xyz/tynn/hoppa/flow/CollectTest.kt
@@ -6,6 +6,7 @@ package xyz.tynn.hoppa.flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
@@ -39,6 +40,53 @@ internal class CollectTest {
                         this,
                         it::add,
                     )
+                }
+            },
+        )
+    }
+
+    @Test
+    fun `collectLatestIn should launch a coroutine`() {
+        runBlocking {
+            val job = flow {
+                while (true) emit(Unit)
+            }.collectLatestIn(this) {}
+
+            assertTrue(job.isActive)
+
+            job.cancel()
+            assertTrue(job.isCancelled)
+            assertFalse(job.isCompleted)
+        }
+    }
+
+    @Test
+    fun `collectLatestIn should collect all values`() {
+        val expected = 1..7
+        assertEquals(
+            expected.toList(),
+            runBlocking {
+                mutableListOf<Int>().also {
+                    expected.asFlow().collectLatestIn(
+                        this,
+                        it::add,
+                    )
+                }
+            },
+        )
+    }
+
+    @Test
+    fun `collectLatestIn should collect latest values`() {
+        val expected = 1..7
+        assertEquals(
+            listOf(7),
+            runBlocking {
+                mutableListOf<Int>().also { list ->
+                    expected.asFlow().collectLatestIn(this) {
+                        yield()
+                        list.add(it)
+                    }
                 }
             },
         )
